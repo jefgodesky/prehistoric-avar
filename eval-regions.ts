eeb9260c5bfb1e13390b31351921b2508282f09e
Jason Godesky <jefgodesky@gmail.com>
2024-10-12 22:25:04 -0400

    Add new task: eval

--- eval-regions.ts
+++ eval-regions.ts
@@ -1,4 +1,5 @@
 import { DOMParser } from 'npm:xmldom'
+import { parse, stringify } from 'npm:yaml'
 import commandsToArea from './commands-to-area.ts'
 import getCarryingCapacityScore from './get-carrying-capacity-score.ts'
 import getRegionIdList from './get-region-id-list.ts'
@@ -30,6 +31,31 @@ export const evalRegions = (input: string, regions: Record<string, IRegion>): IR
   }
 
   return data
+}
+
+if (import.meta.main) {
+  const main = async (): Promise<void> => {
+    if (Deno.args.length !== 2) {
+      console.error('Usage: deno task eval <map> <output>')
+      Deno.exit(1)
+    }
+
+    const [map, output] = Deno.args
+    try {
+      const input = await Deno.readTextFile(`./maps/${map}.svg`)
+      const regionsYAML = await Deno.readTextFile('regions.yml')
+      const regions = parse(regionsYAML) as Record<string, IRegion>
+      const result = evalRegions(input, regions)
+      const resultYAML = stringify(result)
+      await Deno.writeTextFile(`./${output}.yml`, resultYAML)
+      console.log(`Region evaluation successfully written to ./${output}.yml`)
+    } catch (error) {
+      console.error('Error: ', error)
+      Deno.exit(1)
+    }
+  }
+
+  main()
 }
 
 export default evalRegions
