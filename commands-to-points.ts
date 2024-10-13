d30c3ecdb81eec8e9a18d81af86c78ea6b2a2fcc
Jason Godesky <jefgodesky@gmail.com>
2024-10-13 17:16:24 -0400

    commandToPoints: Use approximateCurves
    The approximateCurves method replaces curves with straight lines, so that our command path is true polygon, and we can find meaningful points.

--- commands-to-points.ts
+++ commands-to-points.ts
@@ -1,5 +1,6 @@
 import { SVGPathData, SVGPathDataParser } from 'svg-pathdata'
 import { IPoint } from './index.t.ts'
+import approximateCurves from './approximate-curves.ts'
 import coordsToPoint from './coords-to-point.ts'
 
 const commandsToPoints = (d: string): IPoint[] => {
@@ -10,7 +11,7 @@ const commandsToPoints = (d: string): IPoint[] => {
     SVGPathData.MOVE_TO,
     SVGPathData.LINE_TO
   ]
-  const commands = parser.parse(d)
+  const commands = parser.parse(approximateCurves(d))
     .filter(cmd => relevantCommandTypes.includes(cmd.type as number))
     .filter(cmd => 'x' in cmd && 'y' in cmd)
   return commands.map(cmd => coordsToPoint(cmd.x, cmd.y))
