ac8c4d63cff8deac032fa89c44a0c9bab80ff863
Jason Godesky <jefgodesky@gmail.com>
2024-10-14 01:42:30 -0400

    Refactor evalRegions

--- eval-regions.test.ts
+++ eval-regions.test.ts
@@ -5,17 +5,6 @@ import type { IRegion } from './index.t.ts'
 import evalRegions from './eval-regions.ts'
 
 describe('evalRegions', () => {
-  const input = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="1728" height="901">
-      <g id="F01" class="region forest boreal">
-        <path class="body" d="M0,0 L100,0 L100,100 L0,100 Z" />
-      </g>
-      <g id="D01" class="region desert hot">
-        <path class="body" d="M200,200 L300,200 L300,300 L200,300 Z" />
-      </g>
-    </svg>
-  `
-
   const regions = `
 Boreal forests:
   Class names:
@@ -35,38 +24,26 @@ Hot deserts:
 
   const data = parse(regions) as Record<string, IRegion>
 
-  it('evaluates the correct number of regions', () => {
-    const result = evalRegions(input, data)
+  it('evaluates the correct number of regions', async () => {
+    const result = await evalRegions(data)
     expect(Object.keys(result).length).toBe(2)
   })
 
-  it('correctly assigns tags to regions', () => {
-    const result = evalRegions(input, data)
+  it('correctly assigns tags to regions', async () => {
+    const result = await evalRegions(data)
     expect(result.F01.tags).toEqual(['forest', 'boreal'])
     expect(result.D01.tags).toEqual(['desert', 'hot'])
   })
 
-  it('calculates area for each region', () => {
-    const result = evalRegions(input, data)
-    expect(result.F01.area).toBe(527461)
-    expect(result.D01.area).toBe(2326880)
+  it('calculates area for each region', async () => {
+    const result = await evalRegions(data)
+    expect(result.F01.area).toBe(1272244)
+    expect(result.D01.area).toBe(2688510)
   })
 
-  it('calculates carrying capacity correctly', () => {
-    const result = evalRegions(input, data)
-    expect(result.F01.carryingCapacity).toBe(316476)
-    expect(result.D01.carryingCapacity).toBe(465376)
-  })
-
-  it('skips regions not in our regions data', () => {
-    const inputWithUnknownRegion = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="1728" height="901">
-        <g id="U01" class="region unknown">
-          <path class="body" d="M0,0 L100,0 L100,100 L0,100 Z" />
-        </g>
-      </svg>
-    `
-    const result = evalRegions(inputWithUnknownRegion, data)
-    expect(result).toEqual({})
+  it('calculates carrying capacity correctly', async () => {
+    const result = await evalRegions(data)
+    expect(result.F01.carryingCapacity).toBe(152669)
+    expect(result.D01.carryingCapacity).toBe(107540)
   })
 })
--- eval-regions.ts
+++ eval-regions.ts
@@ -1,32 +1,19 @@
-import { DOMParser } from 'npm:xmldom'
-import { parse, stringify } from 'npm:yaml'
-import commandsToArea from './commands-to-area.ts'
-import getCarryingCapacityScore from './get-carrying-capacity-score.ts'
+import { parse, stringify } from 'yaml'
+import calculateArea from './calculate-area.ts'
 import calculateCarryingCapacity from './calculate-carrying-capacity.ts'
-import getRegionIdList from './get-region-id-list.ts'
 import type { IRegion, IRegionData } from './index.t.ts'
 
-export const evalRegions = (input: string, regions: Record<string, IRegion>): IRegionData => {
-  const idList = getRegionIdList(regions)
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(input, 'text/xml')
-  const groups = Array.from(doc.getElementsByTagName('g')) as Element[]
+export const evalRegions = async (regions: Record<string, IRegion>): Promise<IRegionData> => {
   const data: IRegionData = {}
 
-  for (const group of groups) {
-    const id = group.getAttribute('id')
-    const classes: string[] = (group.getAttribute('class') ?? '').split(' ')
-    if (id && idList.includes(id) && classes.includes('region')) {
-      const tags = classes.filter(tag => tag !== 'region')
-      const paths = group.getElementsByTagName('path')
-      const body = Array.from(paths).find(path => path.getAttribute('class') === 'body')
-      if (body) {
-        const pathData = body.getAttribute('d') ?? ''
-        const areaInSquareKm = commandsToArea(pathData)
-        const area = Math.round(areaInSquareKm)
-        const carryingCapacity = calculateCarryingCapacity(area, getCarryingCapacityScore(tags, regions))
-        data[id] = { tags, area, carryingCapacity }
-      }
+  for (const regionType in regions) {
+    const tags = regions[regionType]['Class names'].slice()
+    const score = regions[regionType]['Carrying capacity score']
+
+    for (const id of regions[regionType].Regions) {
+      const area = await calculateArea(id)
+      const carryingCapacity = calculateCarryingCapacity(area, score)
+      data[id] = { tags, area, carryingCapacity}
     }
   }
 
@@ -35,17 +22,16 @@ export const evalRegions = (input: string, regions: Record<string, IRegion>): IR
 
 if (import.meta.main) {
   const main = async (): Promise<void> => {
-    if (Deno.args.length !== 2) {
-      console.error('Usage: deno task eval <map> <output>')
+    if (Deno.args.length !== 1) {
+      console.error('Usage: deno task eval <output>')
       Deno.exit(1)
     }
 
-    const [map, output] = Deno.args
+    const [output] = Deno.args
     try {
-      const input = await Deno.readTextFile(`./maps/${map}.svg`)
       const regionsYAML = await Deno.readTextFile('regions.yml')
       const regions = parse(regionsYAML) as Record<string, IRegion>
-      const result = evalRegions(input, regions)
+      const result = await evalRegions(regions)
       const resultYAML = stringify(result)
       await Deno.writeTextFile(`./${output}.yml`, resultYAML)
       console.log(`Region evaluation successfully written to ./${output}.yml`)
