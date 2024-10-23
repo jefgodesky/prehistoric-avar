6482616928b41449b000845be6fa64f8a8f45472
Jason Godesky <jefgodesky@gmail.com>
2024-10-23 17:37:54 -0400

    getAdjacencyList

---
+++ get-adjacency-list.test.ts
@@ -0,0 +1,30 @@
+import { describe, it } from 'jsr:@std/testing/bdd'
+import { expect } from 'jsr:@std/expect'
+import { LAYER } from './layer.ts'
+import getAdjacencyList from './get-adjacency-list.ts'
+
+describe('getAdjacencyList', () => {
+  const data = { F01: ['F02'], F02: ['F01'] }
+
+  it('defaults to the surface list', () => {
+    const result = getAdjacencyList('F01', data)
+    expect(result.length).toBe(2)
+    expect(result).toContain('FC01')
+    expect(result).toContain('FS02')
+  })
+
+  it('can get the near-surface list', () => {
+    const result = getAdjacencyList('F01', data, LAYER.NEAR_SURFACE)
+    expect(result.length).toBe(3)
+    expect(result).toContain('FS01')
+    expect(result).toContain('FD01')
+    expect(result).toContain('FC02')
+  })
+
+  it('can get the World Below list', () => {
+    const result = getAdjacencyList('F01', data, LAYER.WORLD_BELOW)
+    expect(result.length).toBe(2)
+    expect(result).toContain('FC01')
+    expect(result).toContain('FD02')
+  })
+})
---
+++ get-adjacency-list.ts
@@ -0,0 +1,19 @@
+import { LAYER, LAYER as LAYERS } from './layer.ts'
+import makeLayerId from './make-layer-id.ts'
+
+const getAdjacencyList = (id: string, data: { [key: string]: string[] }, layer: LAYERS = LAYER.SURFACE): string[] => {
+  let regions: string[] = []
+  const s = makeLayerId(id, LAYER.SURFACE)
+  const c = makeLayerId(id, LAYER.NEAR_SURFACE)
+  const d = makeLayerId(id, LAYER.WORLD_BELOW)
+
+  if (layer === LAYER.SURFACE || layer === LAYER.WORLD_BELOW) regions = [c]
+  if (layer === LAYER.NEAR_SURFACE) regions = [s, d]
+
+  return [
+    ...regions,
+    ...data[id].map(region => makeLayerId(region, layer))
+  ]
+}
+
+export default getAdjacencyList
