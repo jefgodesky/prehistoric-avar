1079272b80dc69ea2fc61de46ce27b37e70b5d80
Jason Godesky <jefgodesky@gmail.com>
2024-10-27 02:06:27 -0400

    Index to export regions

---
+++ instances/regions/index.ts
@@ -0,0 +1,260 @@
+import type { Emitter } from '../../index.d.ts'
+import Region from '../../classes/Region.ts'
+
+import DC01 from './DC01.ts'
+import DD01 from './DD01.ts'
+import DS01 from './DS01.ts'
+import FC01 from './FC01.ts'
+import FC02 from './FC02.ts'
+import FC03 from './FC03.ts'
+import FC04 from './FC04.ts'
+import FC05 from './FC05.ts'
+import FC06 from './FC06.ts'
+import FC07 from './FC07.ts'
+import FC08 from './FC08.ts'
+import FC09 from './FC09.ts'
+import FC10 from './FC10.ts'
+import FC11 from './FC11.ts'
+import FC12 from './FC12.ts'
+import FC13 from './FC13.ts'
+import FC14 from './FC14.ts'
+import FC15 from './FC15.ts'
+import FC16 from './FC16.ts'
+import FC17 from './FC17.ts'
+import FC18 from './FC18.ts'
+import FC19 from './FC19.ts'
+import FC20 from './FC20.ts'
+import FC21 from './FC21.ts'
+import FC22 from './FC22.ts'
+import FC23 from './FC23.ts'
+import FC24 from './FC24.ts'
+import FC25 from './FC25.ts'
+import FC26 from './FC26.ts'
+import FC27 from './FC27.ts'
+import FC28 from './FC28.ts'
+import FC29 from './FC29.ts'
+import FC30 from './FC30.ts'
+import FC31 from './FC31.ts'
+import FC32 from './FC32.ts'
+import FD01 from './FD01.ts'
+import FD02 from './FD02.ts'
+import FD03 from './FD03.ts'
+import FD04 from './FD04.ts'
+import FD05 from './FD05.ts'
+import FD06 from './FD06.ts'
+import FD07 from './FD07.ts'
+import FD08 from './FD08.ts'
+import FD09 from './FD09.ts'
+import FD10 from './FD10.ts'
+import FD11 from './FD11.ts'
+import FD12 from './FD12.ts'
+import FD13 from './FD13.ts'
+import FD14 from './FD14.ts'
+import FD15 from './FD15.ts'
+import FD16 from './FD16.ts'
+import FD17 from './FD17.ts'
+import FD18 from './FD18.ts'
+import FD19 from './FD19.ts'
+import FD20 from './FD20.ts'
+import FD21 from './FD21.ts'
+import FD22 from './FD22.ts'
+import FD23 from './FD23.ts'
+import FD24 from './FD24.ts'
+import FD25 from './FD25.ts'
+import FD26 from './FD26.ts'
+import FD27 from './FD27.ts'
+import FD28 from './FD28.ts'
+import FD29 from './FD29.ts'
+import FD30 from './FD30.ts'
+import FD31 from './FD31.ts'
+import FD32 from './FD32.ts'
+import FS01 from './FS01.ts'
+import FS02 from './FS02.ts'
+import FS03 from './FS03.ts'
+import FS04 from './FS04.ts'
+import FS05 from './FS05.ts'
+import FS06 from './FS06.ts'
+import FS07 from './FS07.ts'
+import FS08 from './FS08.ts'
+import FS09 from './FS09.ts'
+import FS10 from './FS10.ts'
+import FS11 from './FS11.ts'
+import FS12 from './FS12.ts'
+import FS13 from './FS13.ts'
+import FS14 from './FS14.ts'
+import FS15 from './FS15.ts'
+import FS16 from './FS16.ts'
+import FS17 from './FS17.ts'
+import FS18 from './FS18.ts'
+import FS19 from './FS19.ts'
+import FS20 from './FS21.ts'
+import FS21 from './FS21.ts'
+import FS22 from './FS22.ts'
+import FS23 from './FS23.ts'
+import FS24 from './FS24.ts'
+import FS25 from './FS25.ts'
+import FS26 from './FS26.ts'
+import FS27 from './FS27.ts'
+import FS28 from './FS28.ts'
+import FS29 from './FS29.ts'
+import FS30 from './FS30.ts'
+import FS31 from './FS31.ts'
+import FS32 from './FS32.ts'
+import GC01 from './GC01.ts'
+import GC02 from './GC02.ts'
+import GC03 from './GC03.ts'
+import GC04 from './GC04.ts'
+import GC05 from './GC05.ts'
+import GC06 from './GC06.ts'
+import GC07 from './GC07.ts'
+import GC08 from './GC08.ts'
+import GC09 from './GC09.ts'
+import GC10 from './GC10.ts'
+import GC11 from './GC11.ts'
+import GC12 from './GC12.ts'
+import GC13 from './GC13.ts'
+import GD01 from './GD01.ts'
+import GD02 from './GD02.ts'
+import GD03 from './GD03.ts'
+import GD04 from './GD04.ts'
+import GD05 from './GD05.ts'
+import GD06 from './GD06.ts'
+import GD07 from './GD07.ts'
+import GD08 from './GD08.ts'
+import GD09 from './GD09.ts'
+import GD10 from './GD10.ts'
+import GD11 from './GD11.ts'
+import GD12 from './GD12.ts'
+import GD13 from './GD13.ts'
+import GS01 from './GS01.ts'
+import GS02 from './GS02.ts'
+import GS03 from './GS03.ts'
+import GS04 from './GS04.ts'
+import GS05 from './GS05.ts'
+import GS06 from './GS06.ts'
+import GS07 from './GS07.ts'
+import GS08 from './GS08.ts'
+import GS09 from './GS09.ts'
+import GS10 from './GS10.ts'
+import GS11 from './GS11.ts'
+import GS12 from './GS12.ts'
+import GS13 from './GS13.ts'
+import MC01 from './MC01.ts'
+import MC02 from './MC02.ts'
+import MC03 from './MC03.ts'
+import MC04 from './MC04.ts'
+import MC05 from './MC05.ts'
+import MC06 from './MC06.ts'
+import MC07 from './MC07.ts'
+import MC08 from './MC08.ts'
+import MC09 from './MC09.ts'
+import MC10 from './MC10.ts'
+import MC11 from './MC11.ts'
+import MC12 from './MC12.ts'
+import MC13 from './MC13.ts'
+import MC14 from './MC14.ts'
+import MD01 from './MD01.ts'
+import MD02 from './MD02.ts'
+import MD03 from './MD03.ts'
+import MD04 from './MD04.ts'
+import MD05 from './MD05.ts'
+import MD06 from './MD06.ts'
+import MD07 from './MD07.ts'
+import MD08 from './MD08.ts'
+import MD09 from './MD09.ts'
+import MD10 from './MD10.ts'
+import MD11 from './MD11.ts'
+import MD12 from './MD12.ts'
+import MD13 from './MD13.ts'
+import MD14 from './MD14.ts'
+import MS01 from './MS01.ts'
+import MS02 from './MS02.ts'
+import MS03 from './MS03.ts'
+import MS04 from './MS04.ts'
+import MS05 from './MS05.ts'
+import MS06 from './MS06.ts'
+import MS07 from './MS07.ts'
+import MS08 from './MS08.ts'
+import MS09 from './MS09.ts'
+import MS10 from './MS10.ts'
+import MS11 from './MS11.ts'
+import MS12 from './MS12.ts'
+import MS13 from './MS13.ts'
+import MS14 from './MS14.ts'
+import PC01 from './PC01.ts'
+import PC02 from './PC02.ts'
+import PC03 from './PC03.ts'
+import PD01 from './PD01.ts'
+import PD02 from './PD02.ts'
+import PD03 from './PD03.ts'
+import PS01 from './PS01.ts'
+import PS02 from './PS02.ts'
+import PS03 from './PS03.ts'
+
+const regionData = [
+  DS01, DC01, DD01,
+  FS01, FS02, FS03, FS04, FS05, FS06, FS07, FS08,
+  FS09, FS10, FS11, FS12, FS13, FS14, FS15, FS16,
+  FS17, FS18, FS19, FS20, FS21, FS22, FS23, FS24,
+  FS25, FS26, FS27, FS28, FS29, FS30, FS31, FS32,
+  FC01, FC02, FC03, FC04, FC05, FC06, FC07, FC08,
+  FC09, FC10, FC11, FC12, FC13, FC14, FC15, FC16,
+  FC17, FC18, FC19, FC20, FC21, FC22, FC23, FC24,
+  FC25, FC26, FC27, FC28, FC29, FC30, FC31, FC32,
+  FD01, FD02, FD03, FD04, FD05, FD06, FD07, FD08,
+  FD09, FD10, FD11, FD12, FD13, FD14, FD15, FD16,
+  FD17, FD18, FD19, FD20, FD21, FD22, FD23, FD24,
+  FD25, FD26, FD27, FD28, FD29, FD30, FD31, FD32,
+  GS01, GS02, GS03, GS04, GS05, GS06, GS07, GS08,
+  GS09, GS10, GS11, GS12, GS13,
+  GC01, GC02, GC03, GC04, GC05, GC06, GC07, GC08,
+  GC09, GC10, GC11, GC12, GC13,
+  GD01, GD02, GD03, GD04, GD05, GD06, GD07, GD08,
+  GD09, GD10, GD11, GD12, GD13,
+  MS01, MS02, MS03, MS04, MS05, MS06, MS07, MS08,
+  MS09, MS10, MS11, MS12, MS13, MS14,
+  MC01, MC02, MC03, MC04, MC05, MC06, MC07, MC08,
+  MC09, MC10, MC11, MC12, MC13, MC14,
+  MD01, MD02, MD03, MD04, MD05, MD06, MD07, MD08,
+  MD09, MD10, MD11, MD12, MD13, MD14,
+  PS01, PS02, PS03,
+  PC01, PC02, PC03,
+  PD01, PD02, PD03
+]
+
+const getRegions = (emitter: Emitter): Region[] => {
+  return regionData.map(data => new Region(emitter, data))
+}
+
+export {
+  DS01, DC01, DD01,
+  FS01, FS02, FS03, FS04, FS05, FS06, FS07, FS08,
+  FS09, FS10, FS11, FS12, FS13, FS14, FS15, FS16,
+  FS17, FS18, FS19, FS20, FS21, FS22, FS23, FS24,
+  FS25, FS26, FS27, FS28, FS29, FS30, FS31, FS32,
+  FC01, FC02, FC03, FC04, FC05, FC06, FC07, FC08,
+  FC09, FC10, FC11, FC12, FC13, FC14, FC15, FC16,
+  FC17, FC18, FC19, FC20, FC21, FC22, FC23, FC24,
+  FC25, FC26, FC27, FC28, FC29, FC30, FC31, FC32,
+  FD01, FD02, FD03, FD04, FD05, FD06, FD07, FD08,
+  FD09, FD10, FD11, FD12, FD13, FD14, FD15, FD16,
+  FD17, FD18, FD19, FD20, FD21, FD22, FD23, FD24,
+  FD25, FD26, FD27, FD28, FD29, FD30, FD31, FD32,
+  GS01, GS02, GS03, GS04, GS05, GS06, GS07, GS08,
+  GS09, GS10, GS11, GS12, GS13,
+  GC01, GC02, GC03, GC04, GC05, GC06, GC07, GC08,
+  GC09, GC10, GC11, GC12, GC13,
+  GD01, GD02, GD03, GD04, GD05, GD06, GD07, GD08,
+  GD09, GD10, GD11, GD12, GD13,
+  MS01, MS02, MS03, MS04, MS05, MS06, MS07, MS08,
+  MS09, MS10, MS11, MS12, MS13, MS14,
+  MC01, MC02, MC03, MC04, MC05, MC06, MC07, MC08,
+  MC09, MC10, MC11, MC12, MC13, MC14,
+  MD01, MD02, MD03, MD04, MD05, MD06, MD07, MD08,
+  MD09, MD10, MD11, MD12, MD13, MD14,
+  PS01, PS02, PS03,
+  PC01, PC02, PC03,
+  PD01, PD02, PD03,
+  regionData,
+  getRegions
+}
