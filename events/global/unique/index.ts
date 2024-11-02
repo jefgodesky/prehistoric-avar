e5ebe2fd29b23a5f138170978c52bd90ae8201ea
Jason Godesky <jefgodesky@gmail.com>
2024-11-02 08:03:33 -0400

    Package unique global events in an index array for sim use

---
+++ events/global/unique/index.ts
@@ -0,0 +1,23 @@
+import type Simulation from "../../../classes/Simulation.ts"
+
+import death from './death.ts'
+import dragonQueen from './dragon-queen.ts'
+import fs32 from './fs32.ts'
+import language from './language.ts'
+import languageElta from './language-elta.ts'
+import languageHadar from './language-hadar.ts'
+import languageSun from './language-sun.ts'
+import languageTuan from './language-tuan.ts'
+
+const uniqueGlobalEventFunctions: Array<(sim: Simulation, forceEvent?: boolean) => void> = [
+  death,
+  dragonQueen,
+  fs32,
+  language,
+  languageElta,
+  languageHadar,
+  languageSun,
+  languageTuan
+]
+
+export default uniqueGlobalEventFunctions
